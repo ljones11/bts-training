af1e3140b5cda6bf4c83874c3f35b22f35fe69ff
root <root@ip-10-0-1-188.ec2.internal>
2021-01-08 17:04:07 +0000

    made new change

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ try {
 $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
-echo "Connected successfully";
+echo "Connected to Mysql";
 } catch(PDOException $e) {
 echo "Connection failed: " . $e->getMessage();
 }
